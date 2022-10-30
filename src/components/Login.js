5cd72f0d1e4a9de5f02b284bf157ff52f81bfcc7
Karl <karl.lu@outlook.com>
2022-10-30 01:36:04 -0400

    added toLowercase to help search when logging in

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,12 +1,13 @@
 import firebaseConfig from '../firebase';
-import { useState } from 'react';
-import { getDatabase, ref, onValue, push, remove } from 'firebase/database';
+import { getDatabase, ref, onValue } from 'firebase/database';
 
 import useInputState from '../hooks/useInputState';
+import { useNavigate } from 'react-router-dom';
 
-export default function Login({ handleLogIn }) {
+export default function Login({ isLoggedIn, handleLogIn }) {
     const [email, updateEmail, resetEmail] = useInputState('');
     const [password, updatePassword, resetPassword] = useInputState('');
+    const navigate = useNavigate();
     // creates variable that holds our db values
     const database = getDatabase(firebaseConfig);
     // creates a variable that makes reference to our db
@@ -15,15 +16,15 @@ export default function Login({ handleLogIn }) {
     // takes user input for email and password, crossreferences them against all users in db, and logins/redirects if there is a match, else alerts user there is no match
     function handleSubmit(e) {
         e.preventDefault();
-        
+
         onValue(dbRef, (response) => {
             const newState = []
 
             const data = response.val();
             for (let user in data) {
-                if (email === data[user].email && password === data[user].password) {
+                if (email.toLowerCase() === data[user].email.toLowerCase() && password === data[user].password) {
                     newState.push(data[user].email, data[user].name, data[user].investmentAmount, data[user].cash, (data[user].coins ? data[user].coins : []));
-                } 
+                }
             }
 
             if (newState.length > 0) {
@@ -37,15 +38,22 @@ export default function Login({ handleLogIn }) {
     }
 
     return (
-        <div>
-            <h2>Login Page</h2>
-            <form onSubmit={handleSubmit}>
-                <label>Email</label>
-                <input type="email" onChange={(e) => updateEmail(e)} value={email}/>
-                <label>Password</label>
-                <input type="password" onChange={(e) => updatePassword(e)} value={password}/>
-                <button type="submit">Log In</button>
-            </form>            
-        </div>
+        <>
+            {isLoggedIn ? (
+                <>{navigate('/')}</>
+            ) : (
+                <div>
+                    <h2>Login Page</h2>
+                    <form onSubmit={handleSubmit}>
+                        <label>Email</label>
+                        <input type="email" onChange={(e) => updateEmail(e)} value={email} />
+                        <label>Password</label>
+                        <input type="password" onChange={(e) => updatePassword(e)} value={password} />
+                        <button type="submit">Log In</button>
+                    </form>
+                </div>
+            )}
+        </>
+
     )
 }
