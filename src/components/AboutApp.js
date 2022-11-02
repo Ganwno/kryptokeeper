fc514489eb3177a1ddfd7c6310a14901c6f3a923
Karl <karl.lu@outlook.com>
2022-11-02 19:20:03 -0400

    styled and edited content of landing page blurb

--- src/components/AboutApp.js
+++ src/components/AboutApp.js
@@ -4,9 +4,10 @@ export default function AboutApp() {
     return (
         <div className="fittedCard">
             <h2>Curious about crypto?</h2>
-            <p>You've probably heard a lot about the benefits to investing in cryptocurrency. But you're probably also worried about getting into something you haven't had any experience with.</p> 
-            <p><strong>Kryptokeeper</strong> is a cryptocurrency investing simulator where you can try investing in different coins and chart your gains and losses, all with NO RISK.</p>
+            <p className="left">You've heard a lot about the benefits to investing in cryptocurrency, but you're also worried about investing in something in which you have little experience or knowledge.</p>
+            <p className="left"><strong>Kryptokeeper</strong> is a cryptocurrency investment simulator where you can decide how much you want to invest in a variety of coins and chart their performance over time with real and current market prices, with <strong>zero risk</strong> while you're still learning.</p>
             <p><Link to="/register" className="registerLink">Sign up</Link> for a free account today and find out why people are investing!</p>
+            <p><em>None of the risk, none of the reward!</em></p>
         </div>
     )
 }
